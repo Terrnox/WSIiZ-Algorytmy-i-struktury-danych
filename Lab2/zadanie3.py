df46f199f9f28194ad284ff1c44ff94258b53588
Terrnox <81361890+Terrnox@users.noreply.github.com>
2023-03-20 20:26:33 +0100

    Dodano pliki
    Dodano plik Lab1_zadania_23.xlsx oraz zadanie3.py

---
+++ Lab2/zadanie3.py
@@ -0,0 +1,26 @@
+T = []
+n = len(T)
+j = n-1
+while j >= 0:
+    i = 0
+    while i<j:
+        lancuch_liczba1 = str(T[i])
+        lancuch_liczba2 = str(T[i+1])
+        dl1 = len(lancuch_liczba1)
+        dl2 = len(lancuch_liczba2)
+        dl_min = min(dl1, dl2)
+        for k in range(dl_min):
+            if int(lancuch_liczba1[k]) < int(lancuch_liczba2[k]):
+                break
+            elif int(lancuch_liczba1[k]) > int(lancuch_liczba2[k]):
+                tmp = T[i]
+                T[i] = T[i+1]
+                print("tmp ",tmp)
+                print("T+1 ", T[i + 1])
+                T[i+1] = tmp
+                break
+        print(T)
+        i+=1
+    j-=1
+
+print(T)
